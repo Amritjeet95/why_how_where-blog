8201e8dfced316b7380b3ec0b17bf996ad3b9ed6
Amritjeet <amrit.warwal@gmail.com>
2022-02-23 15:57:52 -0500

    create header elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainHeader />
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MainHeader = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Blogs</li>
+                <li>Suggestion Box</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default MainHeader;
